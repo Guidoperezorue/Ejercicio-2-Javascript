bab41705ccd89ac0a8682aa3c0360693879f06dd
Guidoperezorue <78152166+Guidoperezorue@users.noreply.github.com>
2022-09-15 20:16:50 -0300

    Add files via upload

---
+++ Ejercicio 2 Javascript.js
@@ -0,0 +1,14 @@
+const librofav = {
+    titulo: "Cosmos",
+    autor: "Richard Tarnas",
+    fecha: new Date(1980, 5, 5),
+    url: "www.wikipedia.org/wiki/Richard_Tarnas",
+}
+
+const lista = {
+    nombre: "Guido",
+    edad: 32,
+    desarrollador: true,
+    "fecha-nacimiento": new Date(1990, 6, 11),
+    "libro-favorito": librofav,
+}
